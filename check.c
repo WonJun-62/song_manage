b057195c087613926a720d5f3a193aeb2daffea0
WonJun-62 <132991365+WonJun-62@users.noreply.github.com>
2024-04-17 00:43:53 +0900

    4/17 무결성처리 추가

--- check.c
+++ check.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include "header.h"
 
-FILE* txttest = fopen("song_list.txt", "r");
-    if (txttest == NULL) {
-        printf("메뉴 정보 데이터 파일을 새로 생성했습니다.");
-        Sleep(1000);
-        system("cls");
-        FILE* txttest = fopen("song_list.txt", "w");
-        fclose(txttest);
-    }
-    else {
-        fclose(txttest);
-    }
+void check(){ // song_list.txt 파일이 있는지 확인
+    FILE* txttest = fopen("song_list.txt", "r");
+        if (txttest == NULL) {      //없으면 새로 생성
+            printf("노래리스트 데이터 파일이 생성되었습니다.");
+            Sleep(1500);
+            system("cls");          //1.5초 후에 프롬프트 창 비우기
+            FILE* txttest = fopen("song_list.txt", "w");
+            fclose(txttest);
+        }
+        else {                      //있으면
+            // printf("확인");
+            fclose(txttest);
+        }
+}
--- song.c
+++ song.c
@@ -3,26 +3,14 @@
 #include "song_list.c"
 #include "search.c"
 #include "check.c"
-#include <stdio.h>
 
 #define STRING_SIZE 256
-
-// struct Song { //구조체
-//     char title[STRING_SIZE];    //제목
-//     char singer[STRING_SIZE];   //가수
-//     char composer[STRING_SIZE]; //작곡가
-//     char lyricist[STRING_SIZE]; //작사가
-//     char genre[STRING_SIZE];    //장르
-//     char playtime[STRING_SIZE]; //재생시간
-//     char album[STRING_SIZE];    //앨범명
-//     char release[STRING_SIZE];  //앨범출시날짜
-
-// };
 
 int main(){
     SetConsoleOutputCP(CP_UTF8);
     int mode;
     int err = 0;
+    check();
     while (1)
     {
         if (err == 0) {
